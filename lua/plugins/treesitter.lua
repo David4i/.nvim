44101f7b0310a07e352f03d91fb5b83e40b01b50
David <notdavidzin@gmail.com>
2024-11-22 19:58:44 -0300

    feat(treesitter): some changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,48 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
+        "jsdoc",
         "css",
         "scss",
         "html",
         "javascript",
         "json",
         "lua",
-        "markdown",
-        "markdown_inline",
         "python",
-        "query",
-        "regex",
         "tsx",
         "typescript",
         "vim",
         "yaml",
         "vue",
       },
-    },
-  },
+
+      sync_install = false,
+
+      auto_install = true,
+
+      indent = {
+        enable = true,
+      },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown" },
+      },
+    })
+
+    local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    treesitter_parser_config.templ = {
+      install_info = {
+        url = "https://github.com/vrischmann/tree-sitter-templ.git",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "master",
+      },
+    }
+
+    vim.treesitter.language.register("templ", "templ")
+  end,
 }
